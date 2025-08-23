37b0696ce1a461516127e6bbdf201d5f0038a88a
Supun Nethsara <supunmax663@gmail.com>
2025-08-22 23:06:17 -0700

    Make controller and model tables to a shipping details process

---
+++ app/Http/Controllers/ShipingPaymentsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShipingPayments;
+use Illuminate\Http\Request;
+
+class ShipingPaymentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ShipingPayments $shipingPayments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ShipingPayments $shipingPayments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ShipingPayments $shipingPayments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ShipingPayments $shipingPayments)
+    {
+        //
+    }
+}
